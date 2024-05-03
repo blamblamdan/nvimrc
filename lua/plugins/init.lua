eb2292a4e12a3d7f6854c78c5653e7f6d403a4b4
Daniel Stapleton <blamblamdan@gmail.com>
2024-05-03 12:48:21 +0100

    Update plugins\init to table lookup

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -3,23 +3,20 @@ local dir = vim.fn.stdpath("config") .. "/lua/plugins"
 local luafiles = vim.split(vim.fn.glob(dir .."/*"), '\n', {trimempty=true})
 local modules = {}
 
-local excluded = { -- Excluded module names
-	"init",
-	"lazy",
-	"presence"
+-- Table for faster lookup of missing keys
+local excluded = { -- Excluded module names. 'init' is mandatory
+ init = true,
+	lazy = true,
+	presence = true
 }
 
 local function is_excluded(name)
-	for _, value in ipairs(excluded) do
-		if value == name then
-			return true
-		end
-	end
-	return false
+    excluded[name] == nil
 end
 
 for _, plugname in ipairs(luafiles) do
 	-- Get file name
+ -- Filters against *%.txt.+
 	local name = plugname:match("\\([^\\%.]*).lua$")
 	if (name ~= nil) and not is_excluded(name)  then
 		modules[#modules+1] = require("plugins." .. name)
